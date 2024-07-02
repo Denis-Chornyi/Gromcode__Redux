d261d4a4465cf2c1c68ed39147ed538bf4584ff9
Denis Chornyi <chornyi.den@gmail.com>
2024-07-02 17:46:06 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { themes, ThemeContext } from './themes-context';
 
 class App extends React.PureComponent {
   state = {
-    theme: themes.dark
+    theme: themes.light
   };
 
   toggleTheme = () => {

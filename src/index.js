6b90f1c70f0ee5a6034c559dff19cee19de0bc7e
Denis Chornyi <chornyi.den@gmail.com>
2024-07-03 22:41:15 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
-import './index.scss';
-import store, { increment, decrement, reset } from './store.js';
-
-const resultElem = document.querySelector('.counter__result');
-const decrementBtn = document.querySelector('[data-action="decrement"]');
-const resetBtn = document.querySelector('[data-action="reset"]');
-const incrementBtn = document.querySelector('[data-action="increment"]');
-
-const onIncrement = () => {
-  store.dispatch(increment());
-};
-
-const onDecrement = () => {
-  store.dispatch(decrement());
-};
-
-const onReset = () => {
-  store.dispatch(reset());
-};
-
-incrementBtn.addEventListener('click', onIncrement);
-decrementBtn.addEventListener('click', onDecrement);
-resetBtn.addEventListener('click', onReset);
+import store from './store.js';
+import { addUser, deleteUser } from './users.actions.js';
 
 store.subscribe(() => {
-  const state = store.getState();
-  const currentValue = state.history.reduce((acc, value) => acc + parseInt(value, 10), 0);
-  const historyString = state.history.join('');
-  resultElem.textContent = state.history.length === 0 ? '' : `${historyString} = ${currentValue}`;
+  console.log(store.getState());
 });
+
+
+store.dispatch(addUser({ id: 76, name: 'Sarah' }));
+store.dispatch(deleteUser(76));

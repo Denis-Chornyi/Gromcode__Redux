1cd2ba631d4d6f649f55a70000761a35f0cb1ece
Denis Chornyi <chornyi.den@gmail.com>
2024-07-05 10:47:39 +0300

    fix

--- src/user.reducer.js
+++ src/user.reducer.js
@@ -1,19 +1,11 @@
 import { SET_USER, REMOVE_USER } from './user.actions.js';
 
-const initialState = null;
-
-const userReducer = (state = initialState, action) => {
+export const userReducer = (state = null, action) => {
   switch (action.type) {
     case SET_USER:
-      return {
-        ...state,
-        user: action.payload
-      };
+      return action.payload.userData;
     case REMOVE_USER:
-      return {
-        ...state,
-        user: null
-      };
+      return null;
     default:
       return state;
   }

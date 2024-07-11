a16892817b28a51ddd6ee0a60818bfa2f9beadef
Denis Chornyi <chornyi.den@gmail.com>
2024-07-11 11:04:13 +0300

    fix

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { taskReduser } from './tasks/tasks.reducer';
 
 // const logger = store => next => action => {

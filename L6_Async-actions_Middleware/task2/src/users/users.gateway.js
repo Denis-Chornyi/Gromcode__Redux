5dd51af737ffc562558c94243e18548f31174773
Denis Chornyi <153118960+Denis-Chornyi@users.noreply.github.com>
2024-08-02 12:06:52 +0300

    Delete L6_Async-actions_Middleware directory

--- L6_Async-actions_Middleware/task2/src/users/users.gateway.js
+++
@@ -1,9 +0,0 @@
-const babelUrl = 'https://api.github.com/users';
-
-export const getUserData = userName =>
-  fetch(`${babelUrl}/${userName}`).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error();
-  });

7f93bbe05cd434391952c874f676a56733df14b5
Denis Chornyi <153118960+Denis-Chornyi@users.noreply.github.com>
2024-08-02 12:06:27 +0300

    Delete L3_Combining-reducers directory

--- L3_Combining-reducers/task3/src/language.actions.js
+++
@@ -1,9 +0,0 @@
-export const EN = 'LANGUAGE/EN';
-export const ES = 'LANGUAGE/ES';
-export const JP = 'LANGUAGE/JP';
-
-export const setLanguage = language => {
-  return {
-    type: language === 'en' ? EN : language === 'es' ? ES : JP
-  };
-};

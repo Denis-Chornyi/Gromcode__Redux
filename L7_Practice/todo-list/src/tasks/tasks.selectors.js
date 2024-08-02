7499ed050ac605e9f5e2ccb7ab6e9c1ad58276ab
Denis Chornyi <153118960+Denis-Chornyi@users.noreply.github.com>
2024-08-02 12:07:01 +0300

    Delete L7_Practice/todo-list directory

--- L7_Practice/todo-list/src/tasks/tasks.selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const taskListDataSelector = state => state.tasks.tasksList;
-
-export const sortedTaskListSelector = createSelector([taskListDataSelector], tasksList => {
-  return tasksList.slice().sort((a, b) => a.done - b.done);
-});
